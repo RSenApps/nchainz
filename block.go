3aba07070e874336bc7ed0354a39bbfa739d3d2e
Ryan Senanayake <rsenapps@gmail.com>
2018-04-11 19:53:37 -0400

    add functions to blocks

--- block.go
+++ block.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"strconv"
+	"bytes"
+	"crypto/sha256"
+	"time"
+)
 
 type TokenType uint8
+
 const (
-	BNB    TokenType = iota + 1
+	BNB TokenType = iota + 1
 	BTC
 )
 
@@ -18,46 +25,67 @@ type BlockData interface {
 	bytes() []byte
 }
 
+type RawData struct {
+	Data []byte
+}
+
+func (data RawData) bytes() []byte {
+	return data.Data
+}
+
 type MatchData struct {
 	Matches []Match
 	Cancels []Cancel
 }
 
 type TokenData struct {
-	Orders []Order
-	Cancels []Cancel
+	Orders    []Order
+	Cancels   []Cancel
 	Transfers []Transfer
 }
 
 type Match struct {
-	SellTokenType TokenType
-	BuyTokenType TokenType
-	AmountSold uint64
-	SellerBlockIndex uint64
-	BuyerBlockIndex uint64
+	SellTokenType     TokenType
+	BuyTokenType      TokenType
+	AmountSold        uint64
+	SellerBlockIndex  uint64
+	BuyerBlockIndex   uint64
 	SellerOrderOffset uint8
-	BuyerOrderOffset uint8
-	SellerBlockHash []byte
-	BuyerBlockHash []byte
+	BuyerOrderOffset  uint8
+	SellerBlockHash   []byte
+	BuyerBlockHash    []byte
 }
 
 type Order struct {
-	BuyTokenType TokenType
-	AmountToSell uint64
-	AmountToBuy uint64
+	BuyTokenType  TokenType
+	AmountToSell  uint64
+	AmountToBuy   uint64
 	SellerAddress []byte
-	Signature []byte
+	Signature     []byte
 }
 
 type Transfer struct {
-	Amount uint64
+	Amount      uint64
 	FromAddress []byte
-	ToAddress[]byte
-	Signature []byte
+	ToAddress   []byte
+	Signature   []byte
 }
 
 type Cancel struct {
-	BlockIndex uint64
+	BlockIndex  uint64
 	BlockOffset uint8
-	Signature []byte
+	Signature   []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data.bytes(), timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), RawData{[]byte(data)}, prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
 }
