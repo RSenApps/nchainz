a322f5d9c58798027e88f4789dd3bf898cb016e7
Nicholas Egan <nicholas@egansoft.com>
2018-04-11 20:05:00 -0400

    Set up blockchain.go and added gitignore

--- blockchain.go
+++ blockchain.go
@@ -1,1 +1,17 @@
 package main
+
+type Blockchain struct {
+  blocks []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	genesisBlock := NewBlock("Genesis Block", []byte{})
+	blocks := []*Block{genesisBlock}
+	return &Blockchain{blocks}
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks) - 1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
