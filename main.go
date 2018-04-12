46465f0be857ebaef1c49e6055a7e6dad542fc50
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2018-04-11 20:24:19 -0400

    testing out block

--- main.go
+++ main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+)
+
+func decode(b []byte) string {
+	r := bytes.NewBuffer(b)
+	d := gob.NewDecoder(r)
+
+	var data string
+
+	if d.Decode(&data) == nil {
+		return data
+	} else {
+		return "ERROR"
+	}
+}
+
 func main() {
+	chain := NewBlockchain()
+	chain.AddBlock("Send 1 BNB to Ryan")
+	chain.AddBlock("Send 2 BNB to Lizzie")
+	chain.AddBlock("Send 3 BNB to Nick")
+
+	for _, block := range chain.blocks {
+		fmt.Println("Prev hash:", block.PrevBlockHash)
+		fmt.Println("Data:", block.Data.(string))
+		fmt.Println("Hash:", block.Hash)
+		fmt.Println("---------------------------------------")
+	}
 }
