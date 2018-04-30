e296fbb5ab425d4d0cef4bf5e96e0ddca9fa62e2
Ryan Senanayake <rsenapps@gmail.com>
2018-04-30 01:50:10 -0400

    rollback matches

--- consensus_state.go
+++ consensus_state.go
@@ -66,7 +66,15 @@ func (state *ConsensusState) RollbackTransfer(symbol string, transfer Transfer) 
 	tokenState.balances[transfer.ToAddress] -= transfer.Amount
 }
 
-func (state *ConsensusState) AddMatch(match Match) bool {
+type MatchRollbackInfo struct {
+	OriginalSellOrder Order
+	OriginalBuyOrder Order
+	SellerUnclaimedChangeBuy uint64
+	SellerUnclaimedChangeSell uint64
+	BuyerUnclaimedChangeBuy uint64
+}
+
+func (state *ConsensusState) AddMatch(match Match) (bool, MatchRollbackInfo) {
 	//Check if both buy and sell orders are satisfied by match and that orders are open
 	//add to unconfirmedSell and Buy Matches
 	//remove orders from openOrders
@@ -74,17 +82,17 @@ func (state *ConsensusState) AddMatch(match Match) bool {
 	sellTokenState := state.GetTokenState(match.SellSymbol)
 	buyOrderGen, ok := buyTokenState.openOrders.Load(match.BuyOrderID)
 	if !ok {
-		return false
+		return false, MatchRollbackInfo{}
 	}
 	sellOrderGen, ok := sellTokenState.openOrders.Load(match.SellOrderID)
 	if !ok {
-		return false
+		return false, MatchRollbackInfo{}
 	}
 	buyOrder := buyOrderGen.(Order)
 	sellOrder := sellOrderGen.(Order)
 
 	if sellOrder.AmountToSell < match.AmountSold {
-		return false
+		return false, MatchRollbackInfo{}
 	}
 
 	//sell currency: AmountFromSellOrder = match.AmountSold; AmountToBuyer = match.AmountSold;
@@ -96,13 +104,18 @@ func (state *ConsensusState) AddMatch(match Match) bool {
 	sellPrice := float64(sellOrder.AmountToSell) / float64(sellOrder.AmountToBuy)
 	sellerMinAmountReceived := uint64(math.Floor(sellPrice * float64(match.AmountSold)))
 	if buyerMaxAmountPaid < sellerMinAmountReceived || buyerMaxAmountPaid > buyOrder.AmountToSell {
-		return false
+		return false, MatchRollbackInfo{}
 	}
+
+	var rollbackInfo MatchRollbackInfo
+	rollbackInfo.OriginalBuyOrder = buyOrder
+	rollbackInfo.OriginalSellOrder = sellOrder
 
 	//Trade is between match.AmountSold and buyerMaxAmountPaid
 	//TODO: minerReward := buyerMaxAmountPaid - sellerMinAmountPaid
+	rollbackInfo.SellerUnclaimedChangeBuy = sellerMinAmountReceived
 	buyTokenState.unclaimedFundsLock.Lock()
-	buyTokenState.unclaimedFunds[sellOrder.SellerAddress] += sellerMinAmountReceived
+	buyTokenState.unclaimedFunds[sellOrder.SellerAddress] += rollbackInfo.SellerUnclaimedChangeBuy
 	buyTokenState.unclaimedFundsLock.Unlock()
 
 	sellTokenState.unclaimedFundsLock.Lock()
@@ -115,12 +128,15 @@ func (state *ConsensusState) AddMatch(match Match) bool {
 	buyOrder.AmountToBuy -= match.AmountSold
 	buyOrder.AmountToSell -= buyerMaxAmountPaid
 
+	var deletedOrders []Order
 	if sellOrder.AmountToBuy <= 0 {
 		if sellOrder.AmountToSell > 0 {
+			rollbackInfo.SellerUnclaimedChangeSell = sellOrder.AmountToSell
 			sellTokenState.unclaimedFundsLock.Lock()
-			sellTokenState.unclaimedFunds[sellOrder.SellerAddress] += sellOrder.AmountToSell
+			sellTokenState.unclaimedFunds[sellOrder.SellerAddress] += rollbackInfo.SellerUnclaimedChangeSell
 			sellTokenState.unclaimedFundsLock.Unlock()
 		}
+		deletedOrders = append(deletedOrders, sellOrder)
 		sellTokenState.openOrders.Delete(sellOrder.ID)
 	} else {
 		sellTokenState.openOrders.Store(sellOrder.ID, sellOrder)
@@ -128,19 +144,34 @@ func (state *ConsensusState) AddMatch(match Match) bool {
 
 	if buyOrder.AmountToBuy <= 0 {
 		if buyOrder.AmountToSell > 0 {
+			rollbackInfo.BuyerUnclaimedChangeBuy = buyOrder.AmountToSell
 			buyTokenState.unclaimedFundsLock.Lock()
-			buyTokenState.unclaimedFunds[buyOrder.SellerAddress] += buyOrder.AmountToSell
+			buyTokenState.unclaimedFunds[buyOrder.SellerAddress] += rollbackInfo.BuyerUnclaimedChangeBuy
 			buyTokenState.unclaimedFundsLock.Unlock()
 		}
+		deletedOrders = append(deletedOrders, buyOrder)
 		buyTokenState.openOrders.Delete(buyOrder.ID)
 	} else {
 		buyTokenState.openOrders.Store(buyOrder.ID, buyOrder)
 	}
-	return true
+	return true, rollbackInfo
 }
 
-func (state *ConsensusState) RollbackMatch(match Match) {
+func (state *ConsensusState) RollbackMatch(match Match, rollbackInfo MatchRollbackInfo) {
+	buyTokenState := state.GetTokenState(match.BuySymbol)
+	sellTokenState := state.GetTokenState(match.SellSymbol)
+	buyTokenState.openOrders.Store(match.SellOrderID, rollbackInfo.OriginalBuyOrder)
+	sellTokenState.openOrders.Store(match.BuyOrderID, rollbackInfo.OriginalSellOrder)
 
+	buyTokenState.unclaimedFundsLock.Lock()
+	buyTokenState.unclaimedFunds[rollbackInfo.OriginalSellOrder.SellerAddress] -= rollbackInfo.SellerUnclaimedChangeBuy
+	buyTokenState.unclaimedFunds[rollbackInfo.OriginalBuyOrder.SellerAddress] -= rollbackInfo.BuyerUnclaimedChangeBuy
+	buyTokenState.unclaimedFundsLock.Unlock()
+
+	sellTokenState.unclaimedFundsLock.Lock()
+	sellTokenState.unclaimedFunds[rollbackInfo.OriginalSellOrder.SellerAddress] -= rollbackInfo.SellerUnclaimedChangeSell
+	sellTokenState.unclaimedFunds[rollbackInfo.OriginalBuyOrder.SellerAddress] -= match.AmountSold
+	sellTokenState.unclaimedFundsLock.Unlock()
 }
 
 func (state *ConsensusState) AddCancelOrder(cancelOrder CancelOrder) bool {
