130e3e1b2461c1e483159c1bda81eda6db6f51fa
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2018-05-07 00:09:41 -0400

    added test case that was previously failing

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@ case $1 in
   "RESET")
     rm -rf db
     mkdir db
+    go install
     ;;
   "STARTNODES")    
     go install      
@@ -14,6 +15,16 @@ case $1 in
     dex transfer 500 NATIVE Satoshi x
     for i in `seq 1 10`; do
       (dex transfer 400 NATIVE x Satoshi) &
-    done 
+    done
+    ;;
+  "TEST2")
+    for i in `seq 1 50`; do
+      (dex transfer 100 NATIVE Satoshi x)
+    done
+    dex claim 1000 NATIVE Satoshi 
+    ;; 
+  *)
+    echo "Command not found: try again"
+    ;;
 esac
 
