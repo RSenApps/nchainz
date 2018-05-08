53d7517d501bf98e7fcef167333f122723ad6b9f
Elizabeth Wei <elizabeth.m.wei@gmail.com>
2018-05-08 19:49:17 -0400

    refactored test, added more

--- transactions_test.go
+++ transactions_test.go
@@ -8,6 +8,10 @@ import (
 	"time"
 )
 
+// Example: `go test -run "TestBasicTransfer"`
+// Run `sh config.sh RESET` between tests to reset dbs
+
+// Transfer: Satoshi --500 NATIVE--> x
 func TestBasicTransfer(t *testing.T) {
 	LogRed("Testing: basic transfers")
 
@@ -24,20 +28,87 @@ func TestBasicTransfer(t *testing.T) {
 
 	client.Transfer(500, "NATIVE", "Satoshi", "x")
 
-	for {
-		r1 := client.GetBalance("Satoshi", "NATIVE")
-		r2 := client.GetBalance("x", "NATIVE")
-		if !r1.Success || !r2.Success {
-			t.Fatal("FAILED: basic transfers")
-			return
-		}
-		if r1.Amount == 99999500 && r2.Amount == 500 {
-			LogRed("Passed: basic transfers")
-			return
-		}
-		time.Sleep(100 * time.Millisecond)
+	success := checkBalance(client, []string{"Satoshi", "x"}, []uint64{99999500, 500}, "NATIVE")
+
+	if success {
+		LogRed("Passed: basic transfers")
+	} else {
+		t.Fatal("FAILED: basic transfers")
+	}
+	fmt.Println()
+}
+
+// Transfer: Satoshi --500 NATIVE--> x
+// Transfer: x --500 NATIVE--> Satoshi
+func TestSwapTransfer(t *testing.T) {
+	LogRed("Testing: swap transfers")
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	gob.RegisterName("main.Transfer", Transfer{})
+
+	time.Sleep(100 * time.Millisecond)
+
+	client, err := NewClient()
+	if err != nil {
+		t.Fatal("FAILED: swap transfers")
+		return
 	}
 
-	LogRed("Passed: basic transfers")
+	client.Transfer(500, "NATIVE", "Satoshi", "x")
+	success := checkBalance(client, []string{"Satoshi", "x"}, []uint64{99999500, 500}, "NATIVE")
+	if success {
+		LogRed("Passed: swap transfers")
+	} else {
+		t.Fatal("FAILED: swap transfers")
+	}
+
+	client.Transfer(500, "NATIVE", "x", "Satoshi")
+	success = checkBalance(client, []string{"Satoshi", "x"}, []uint64{100000000, 0}, "NATIVE")
+
+	if success {
+		LogRed("Passed: swap transfers")
+	} else {
+		t.Fatal("FAILED: swap transfers")
+	}
+
 	fmt.Println()
+}
+
+// Multiple client
+// Lots of transfers
+// Transfers that shouldn't work
+// Cyclic transfers
+
+//
+// Helper method to check balances
+//
+func checkBalance(client *Client, users []string, amounts []uint64, symbol string) bool {
+	duration := 100
+	if len(users) != len(amounts) {
+		LogRed("ERROR: Length of users != length of amounts in checkBalance()")
+	}
+
+	for {
+		for i := 0; i < len(users); i++ {
+			r := client.GetBalance(users[i], symbol)
+
+			if !r.Success {
+				return false
+			}
+
+			if r.Amount == amounts[i] {
+				return true
+			} else {
+				break
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+		duration -= 1
+		if duration == 0 {
+			return false
+		}
+	}
+
+	return false
 }
