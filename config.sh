84e9989d1e1f3c2d0ae6c436a9c5633e32392f1d
Nicholas Egan <nicholas@egansoft.com>
2018-05-08 22:23:34 -0400

    hardcoded mainnet seeds in config

--- config.sh
+++ config.sh
@@ -11,6 +11,12 @@ case $1 in
     nchainz create ETH 100000000 5 vit
     nchainz create USD 100000000 2 sam
     ;;
+  "MAINNET")
+    printf "35.172.27.110:5000\n35.174.80.163:5000\n52.87.50.12:5000" > seeds.txt
+    ;;
+  "LOCALNET")
+    printf "localhost:4000\nlocalhost:4001\nlocalhost:4002" > seeds.txt
+    ;;
   *)
     echo "Command not found: try again"
     ;;
