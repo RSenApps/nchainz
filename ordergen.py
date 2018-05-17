943433fe92da93eadaa0431c02878a0a1e3688b3
Nicholas Egan <nicholas@egansoft.com>
2018-05-16 20:04:01 -0400

    put everything under satoshi

--- ordergen.py
+++ ordergen.py
@@ -1,14 +1,22 @@
 import random
+import math
 import os
 import time
 
+i = 0
 while True:
+    i += 1
     price = int(random.normalvariate(1000, 100))
+    ethAmt = 1
+    usdAmt = price * ethAmt
     
     if random.randint(0,1) == 0:
-        print "nchainz order 1 ETH " + str(price) + " USD sam"
-        os.system("nchainz order 1 ETH " + str(price) + " USD sam")
+        command = "nchainz order " + str(ethAmt) + " ETH " + str(usdAmt) + " USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K"
     else:
-        os.system("nchainz order " + str(price) + " USD 1 ETH vit")
+        command = "nchainz order " + str(usdAmt) + " USD " + str(ethAmt) +" ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K"
 
-    time.sleep(1)
+    print command
+    os.system(command)
+
+    delay = -5 * math.cos(i*3.14159/15) + 5
+    time.sleep(4)
