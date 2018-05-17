562ec1fd1e05c2dba3571fb4bb160bc3c6187cfb
Nicholas Egan <nicholas@egansoft.com>
2018-05-16 20:51:50 -0400

    Negancoin and Lizcoin

--- config.sh
+++ config.sh
@@ -8,38 +8,38 @@ case $1 in
     pkill -f "dex"
     ;;
   "MC")
-    nchainz create ETH 100000000 5 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz create USD 100000000 2 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz create NEGAN 100000000 5 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz create LIZ 100000000 2 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
     ;;
   "MAKEMARKET")
-    nchainz order 1 ETH 990 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1100 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 990 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1005 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 850 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1042 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 934 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1023 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 923 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1092 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 929 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1105 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 990 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1100 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 990 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1005 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 850 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1042 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 934 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1023 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 923 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1092 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 929 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1105 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
     ;;
   "TAKEMARKET")
-    nchainz order 1 ETH 1100 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 990 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1090 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 850 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1010 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 900 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1039 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 905 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1017 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 957 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1063 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 963 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 1 ETH 1033 USD 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
-    nchainz order 910 USD 1 ETH 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1100 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 990 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1090 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 850 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1010 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 900 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1039 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 905 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1017 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 957 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1063 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 963 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 1 NEGAN 1033 LIZ 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
+    nchainz order 910 LIZ 1 NEGAN 1CufpgmhVmV7fujYHqFCqUdJe5vwhcc96K
     ;;
   "MAINNET")
     printf "35.172.27.110:5000\n35.174.80.163:5000\n52.87.50.12:5000" > seeds.txt
